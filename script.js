c129618bc34f2a1ff76db4df14d9977614db7644
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-11-03 19:03:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const setup = () => {
+    return {
+        isNavOpen: false,
+
+        billPlan: 'monthly',
+
+        plans: [
+        {
+            name: 'Easy',
+            discretion: 'All the basics for businesses that are just getting started.',
+            price: {
+            monthly: 29,
+            annually: 29 * 12 - 199,
+            },
+            features: ['One project', 'Your dashboard'],
+        },
+        {
+            name: 'Basic',
+            discretion: 'Better for growing businesses that want more customers.',
+            price: {
+            monthly: 59,
+            annually: 59 * 12 - 100,
+            },
+            features: ['Two projects', 'Your dashboard', 'Components included', 'Advanced charts'],
+        },
+        {
+            name: 'Custom',
+            discretion: 'Advanced features for pros who need more customization.',
+            price: {
+            monthly: 139,
+            annually: 139 * 12 - 100,
+            },
+            features: ['Unlimited projects', 'Your dashboard', '+300 Components', 'Chat support'],
+        },
+        ],
+    }
+}
